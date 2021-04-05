04f578fa96452746f1648889de65f641e84c8c22
Martinhos <martinhofesantos@gmail.com>
2021-04-05 21:16:49 +0100

    Enter

--- scripts/AddWineScreen.js
+++ scripts/AddWineScreen.js
@@ -1,31 +1,113 @@
 import 'react-native-gesture-handler';
 import React, { Component, useState } from 'react';
-import { StyleSheet, View, TextInput, Button, Alert, ScrollView, TouchableWithoutFeedback, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Modal, FlatList, Alert, ScrollView, TouchableWithoutFeedback, TouchableOpacity, Text } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 import globalstyles from '../app/config/colors.js'
 import {Keyboard} from 'react-native'
 import Card from '../shared/card'
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 
 export default class AddWine extends Component {
     state = {
         nome: null,
-        regiao: null,
+        Regiao: null,
         ano: null,
-        tipo: null,
+        Tipo: null,
         aval: null,
         periodo_i: null,
         periodo_f: null,
         foto: null,
         preco: null,
         alcool: null,
+        modal_opened: false,
+        selectedProperty: null,
     };
+
+    updateModal(query) {
+        {
+            const db = SQLite.openDatabase('Lista_Vinhos.db');
+            db.transaction((tx) => {
+                tx.executeSql(query, [], (_, ResultSet) => {
+                    //console.log(ResultSet.rows._array.length);
+                    if (ResultSet.rows._array.length !== 0) {
+                        console.log(ResultSet.rows._array)
+                        this.setState({items: ResultSet.rows._array});
+                    }
+                    //console.log(this.state.items)
+                    //console.log(this.props.route);
+                }, (tx, error) => {
+                    Alert.alert(
+                        'Error!',
+                        'Could not Query to Database',
+                        [
+                            {
+                                text: 'OK',
+                            }
+                        ]
+                    );
+                });
+            }, (error) => {
+                Alert.alert(
+                    'Error!',
+                    'Could not open Database',
+                    [
+                        {
+                            text: 'OK',
+                        }
+                    ]
+                );
+            }, () => {
+                //this.render()
+            });
+        }
+    }
 
     render() {
 
+        const Item = ({item, onPress}) => (
+            <TouchableWithoutFeedback
+                onPress={onPress}
+            >
+                <Text style={styles.table_element}>{item[this.state.selectedProperty]}</Text>
+            </TouchableWithoutFeedback>
+        );
+
+        const renderItem = ({item}) => {
+
+            return(
+                <Item
+                    item={item}
+                    onPress={() => {this.setState({[this.state.selectedProperty]: item[this.state.selectedProperty]}); this.setState({modal_opened: false})}}
+                />
+            )
+        };
+
         return(
             <View style={{flex:1}}>
+                <Modal
+                    visible={this.state.modal_opened}
+                    animationType='slide' 
+                    transparent={false}
+                >
+                    <View>
+                        <View style={styles.modal_header}>
+                            <MaterialIcons size={28} name='close' onPress={() => {this.setState({modal_opened: false}); this.setState({selectedProperty: null})}} style={styles.close_icon}/>
+                            <Text style={styles.modal_header_title}>{this.state.selectedProperty === null ? '' : this.state.selectedProperty}</Text>
+                        </View>
+                        <View style={{width: '90%', alignSelf: 'center'}}>
+                            <View style={{marginTop: 25, width:'100%'}}>
+                                <Text style={styles.table_header} >{this.state.selectedProperty}</Text>
+                                <FlatList
+                                    data={this.state.items}
+                                    renderItem={renderItem}
+                                    keyExtractor={item => item.ID.toString()}
+                                />
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
                 <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
                     <View style={globalstyles.container}>
                         <ScrollView>
@@ -39,20 +121,6 @@ export default class AddWine extends Component {
                                 placeholder='Nome do Vinho'
                                 keyboardType='default'
                             />
-                            <View>
-                                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                                    <Text style={styles.label}>Região</Text>
-                                    {this.state.regiao === null ? (
-                                        <TouchableOpacity>
-                                            <Card>
-                                                <Text>Adicionar Região</Text>
-                                            </Card>
-                                        </TouchableOpacity>
-                                    ) : (
-                                        <Text>{this.state.regiao}</Text>
-                                    )}
-                                </View>
-                            </View>
                             <TextInput
                                 style={styles.input}
                                 onChangeText={(value) => {
@@ -61,15 +129,6 @@ export default class AddWine extends Component {
                                 value={this.state.ano}
                                 placeholder='Ano de Produção'
                                 keyboardType='numeric'
-                            />
-                            <TextInput
-                                style={styles.input}
-                                onChangeText={(value) => {
-                                    this.state.tipo = value;
-                                }}
-                                value={this.state.tipo}
-                                placeholder='Tipo de  Vinho'
-                                keyboardType='default'
                             />
                             <TextInput
                                 style={styles.input}
@@ -95,7 +154,7 @@ export default class AddWine extends Component {
                                     this.state.periodo_f = value;
                                 }}
                                 value={this.state.periodo_f}
-                                placeholder='Período Ideal de Consumo - Inicial'
+                                placeholder='Período Ideal de Consumo - Final'
                                 keyboardType='default'
                             />
                             <TextInput
@@ -116,13 +175,39 @@ export default class AddWine extends Component {
                                 placeholder='Graduação Alcoólica'
                                 keyboardType='numeric'
                             />
+                            <View>
+                                <View style={{flexDirection: 'row',}}>
+                                    <Text style={styles.label}>Região</Text>
+                                    {this.state.Regiao === null ? (
+                                        <TouchableOpacity style={{width: '55%'}} onPress={() => {this.setState({modal_opened: true}); this.setState({selectedProperty: 'Regiao'}); this.updateModal('SELECT DISTINCT Regiao, ID FROM Vinhos')}}>
+                                            <Card>
+                                                <Text>Adicionar Região</Text>
+                                            </Card>
+                                        </TouchableOpacity>
+                                    ) : (
+                                        <Text style={styles.propertyText}>{this.state.Regiao}</Text>
+                                    )}
+                                </View>
+                                <View style={{flexDirection: 'row',}}>
+                                    <Text style={styles.label}>Tipo de Vinho</Text>
+                                    {this.state.Tipo === null ? (
+                                        <TouchableOpacity style={{width: '55%'}} onPress={() => {this.setState({modal_opened: true}); this.setState({selectedProperty: 'Tipo'}); this.updateModal('SELECT DISTINCT Tipo, ID FROM Vinhos')}}>
+                                            <Card>
+                                                <Text style={{fontSize: 12}}>Adicionar Tipo de Vinho</Text>
+                                            </Card>
+                                        </TouchableOpacity>
+                                    ) : (
+                                        <Text style={styles.propertyText}>{this.state.Tipo}</Text>
+                                    )}
+                                </View>
+                            </View>
                         </ScrollView>
                         <Button
                             title='Submit'
                             onPress={() => {
                                 const db = SQLite.openDatabase('Lista_Vinhos.db');
                                 db.transaction(async (tx) => {
-                                    tx.executeSql('insert into Vinhos (Regiao, Nome, Ano, Tipo, Aval, Periodo_i, Periodo_f, Foto, Preco, Alcool) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [this.state.regiao, this.state.nome, this.state.ano, this.state.tipo, this.state.aval, this.state.periodo_i, this.state.periodo_f, this.state.foto, this.state.preco, this.state.alcool], (_, ResultSet) => {
+                                    tx.executeSql('insert into Vinhos (Regiao, Nome, Ano, Tipo, Aval, Periodo_i, Periodo_f, Foto, Preco, Alcool) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [this.state.Regiao, this.state.nome, this.state.ano, this.state.Tipo, this.state.aval, this.state.periodo_i, this.state.periodo_f, this.state.foto, this.state.preco, this.state.alcool], (_, ResultSet) => {
                                         Alert.alert('Insert Successful', 'The wine has been successfully inserted into the database', [
                                             {
                                                 text: 'OK',
@@ -152,6 +237,11 @@ export default class AddWine extends Component {
 }
 
 const styles = StyleSheet.create({
+	close_icon: {
+		position: 'absolute',
+		left: 15,
+        top: 5,
+	},
 	input: {
         borderWidth: 1,
 		borderColor: '#ddd',
@@ -168,5 +258,50 @@ const styles = StyleSheet.create({
 		padding: 10,
 		fontSize: 18,
         width: '45%'
+    },
+    modal_header: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingBottom: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 1, height: 5, },
+		shadowOpacity: 1,
+		shadowRadius: 2,
+		elevation: 3,
+		backgroundColor: '#fff',
+    },
+    modal_header_title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    propertyText: {
+        width: '55%',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        borderWidth: 1,
+		borderColor: '#ddd',
+		padding: 10,
+		fontSize: 18,
+		borderRadius: 6,
+    },
+    table_element: {
+        fontSize: 18,
+        padding: 10,
+        width: '100%',
+        borderTopColor: '#fff',
+        borderColor: '#ddd',
+        borderWidth: 1,
+        alignSelf: 'center',
+    },
+    table_header: {
+        fontSize: 18,
+        padding: 10,
+        borderColor: '#ddd',
+        borderWidth: 1,
+        borderTopLeftRadius: 6,
+        borderTopRightRadius: 6,
+        width:'100%',
+        textAlign: 'center',
+        fontWeight: 'bold',
     },
 });
